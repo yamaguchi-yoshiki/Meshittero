ac3ee51326dd52d39302d0262b13fe09b3bba127
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-04-10 11:01:18 +0000

    [Add] 4-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resouce)
+    '/homes/about'
+  end
+
+  def after_sign_out_path_for(resouce)
+    '/homes/about'
+  end
+
   protected
 
   def configure_permitted_parameters
