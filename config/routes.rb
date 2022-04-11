2ed18e73319a3cef0d557c580b9f66f8d7d2c58b
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-04-11 10:39:03 +0000

    [Add] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
-  
-  resouces :post_images, only: [new, index ,show]
+
+  resources :post_images, only: [:new, :create, :index ,:show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
