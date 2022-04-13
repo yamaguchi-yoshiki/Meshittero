ec667a61cb7be80763e82dc78f17529d49059c85
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-04-13 09:35:55 +0000

    [Add] 4-19

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,7 @@
 class PostImage < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorite, dependent: :destroy
 
   has_one_attached :image
 
@@ -11,4 +12,9 @@ class PostImage < ApplicationRecord
     end
     image
   end
+
+  def favorited_by?(user)
+    favorite.exists?(user_id: user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-   resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
